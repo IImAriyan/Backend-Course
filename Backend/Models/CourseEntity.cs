a5fff081559b84661210c82595892b940ebc6d7e
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-26 16:53:17 +0330

    Create Course Entity

---
+++ Backend/Models/CourseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Backend.Models;
+
+[Table(name:"Courses")]
+public class CourseEntity
+{
+    [Key] 
+    [Required]
+    [SwaggerIgnore]
+    public int Id { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "title")]
+    [Required]
+    public string title { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "date")]
+    [Required]
+    public string date { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "Time")]
+    [Required]
+    public string Time { get; set; }
+}
