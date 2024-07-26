fb34008038d3f3156653a31ac6dd3d74d5872ea2
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-26 16:57:31 +0330

    Add Swagger

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -17,6 +18,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
