20c1850545f02e36fa993154d4a0bd99d9411588
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-26 16:56:35 +0330

    Create Db Context

---
+++ Backend/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<CourseEntity> Courses { get; set; }
+
+    public DbSet<StudentsEntity> Students { get; set; }
+
+}
---
+++ Backend/Models/StudentsEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Backend.Models;
+
+[Table(name:"Students")]
+public class StudentsEntity
+{
+    [Key] 
+    [Required]
+    [SwaggerIgnore]
+    public int Id { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "firstName")]
+    [Required]
+    public string firstName { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "lastName")]
+    [Required]
+    public string lastName { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "age")]
+    [Required]
+    public int age { get; set; }
+    
+    [MaxLength(200)]
+    [Display(Name = "forCourse")]
+    [Required]
+    public int forCourse { get; set; }
+}
